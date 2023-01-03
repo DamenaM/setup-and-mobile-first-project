a089ba5f69a150a045a1af44860d3aeb0fcbb114
Mtab <107020397+DamenaM@users.noreply.github.com>
2023-01-03 09:55:12 +0300

    fix error ESlinter

--- Js/main.js
+++ Js/main.js
@@ -1,29 +1,25 @@
+const clickhum = document.getElementById('logoburger');
+const clickclose = document.getElementById('menubackpop');
+const firstcardBack = document.getElementById('firstcardBackid');
+const mainheader = document.getElementById('mylogo-section');
 
-let clickhum = document.getElementById('logoburger');
-let clickclose = document.getElementById('menubackpop');
-let firstcardBack = document.getElementById('firstcardBackid');
-let mainheader=document.getElementById('mylogo-section');
-
-clickhum.addEventListener("click", function() {
-   if (firstcardBack.className = "newmenuback"){
-      mainheader.className="hide"
-      firstcardBack.className = "newmenuback";  
-      clickclose.className = "menuback";
-   }else {
-      mainheader.className="header"
-   }
-
+clickhum.addEventListener('click', () => {
+  if (firstcardBack.className === 'newmenuback') {
+    mainheader.className = 'hide';
+    firstcardBack.className = 'newmenuback';
+    clickclose.className = 'menuback';
+  } else {
+    mainheader.className = 'header';
+  }
 });
 
-
-clickclose.addEventListener("click", function() {
-   if (clickclose.className === "menu-back") {
-      clickclose.className = "hide";
-      firstcardBack.className = "firstcardBack";
-     
-       }else {
-         firstcardBack.className = "firstcardBack";
-         clickclose.className = "hide";
-         mainheader.className="header";
-       }
-}); 
+clickclose.addEventListener('click', () => {
+  if (clickclose.className === 'menu-back') {
+    clickclose.className = 'hide';
+    firstcardBack.className = 'firstcardBack';
+  } else {
+    firstcardBack.className = 'firstcardBack';
+    clickclose.className = 'hide';
+    mainheader.className = 'header';
+  }
+});
